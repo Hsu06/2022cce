2182d4de134d167cd0930f1867eeebdf03b132d6
Hsu06 <112918645+Hsu06@users.noreply.github.com>
2022-09-30 16:20:11 +0800

    電費

--- week0401.cpp
+++ week0401.cpp
@@ -1,1 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int R,P;
+	scanf("%d",&R);
+	if(R<=20) P=80;
+	else if(20<R && R<=50) P=80+(R-20)*3;
+	else if(50<R && R<=100) P=170+(R-50)*5;
+	else P=420+(R-100)*8;
+	printf("P=%d\n",P);
 
+}
